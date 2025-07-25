51c4820d7c18f5a6b7de90f2893fbec802061110
Aditya Rout <routaditya478@gmail.com>
2025-07-25 23:16:31 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -1,29 +1,32 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-let cached = global.mongoose
+const MONGODB_URI = process.env.MONGODB_URI;
 
-if(!cached) {
-    cached = global.mongoose = { conn: null, promise: null }
+if (!MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable');
 }
 
-async function connectDB() {
+let cached = global.mongoose;
 
-    if (cached.com) {
-        return cached.com
-    }
-
-    if (!cached.promise) {
-        const opts = {
-            bufferCommands:false
-        }
-        cached.promise =  mongoose.connect('${process.env.MONGODB_URI/quickcart',opts)).isObjectIdOrHexString(mongoose=>{
-            return mongoose
-        })
-
-
-    }
-    cached.conn = await cached.promise
-    return cached.conn
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
+
+export const connectDB = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
 
 export default connectDB
